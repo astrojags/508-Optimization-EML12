3ceadb9673c8fceea892aae1ea256757b437d17a
jagadam2 <jagadam2@VCL-WS-017>
2025-04-08 12:53:28 -0500

    initial adding test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 508-Optimization-EML12
+
+This is the final project for our AE 508 Optimal Space Trajectories Class!
---
+++ indirect_optimization.m
@@ -0,0 +1,4 @@
+%AE 508 Project Indirect Optimization direct to Periodic Orbit 
+% Sneha, Nitya and Matthew
+
+mu = 398600; %km^3/m^2
